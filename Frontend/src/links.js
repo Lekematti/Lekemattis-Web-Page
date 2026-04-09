b07d01c61829a961bbdc5388e4fd2acbb8a837c5
Leo Koskimäki <103430948+Lekematti@users.noreply.github.com>
2026-04-09 13:57:15 +0300

    changed the preview video for youtube

--- Frontend/src/links.js
+++ Frontend/src/links.js
@@ -4,7 +4,7 @@ export const GITHUB_URL = 'https://github.com/Lekematti';
 export const FEATURED_GITHUB_REPOS = ['TODO', 'Inno'];
 
 export const YOUTUBE_CHANNEL_URL = 'https://www.youtube.com/@lekematti';
-export const YOUTUBE_VIDEO_URL = 'https://www.youtube.com/watch?v=eAgx5muPK7w';
+export const YOUTUBE_VIDEO_URL = 'https://www.youtube.com/watch?v=MUxC_8rCsAU';
 export const YOUTUBE_OEMBED_URL = `https://www.youtube.com/oembed?format=json&url=${encodeURIComponent(
   YOUTUBE_VIDEO_URL,
 )}`;
